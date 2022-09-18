fd258f73ecbef183051ffa5a4e7399acbc9b8c95
IlyaIIS <ilya1221221@mail.ru>
2022-09-18 23:18:16 +0500

    2.1 Добавлена Bootstrap вёрстка

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,19 +1,28 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
+    private Date creationDate;
+
     private Integer likes;
 
     public String getText() {
         return text;
     }
 
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
     public Integer getLikes() {
         return likes;
     }
 
-    public Post(String text) {
+    public Post(String text, Date date) {
         this.text = text;
         likes = 0;
+        creationDate = date;
     }
 }
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -4,6 +4,7 @@ import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -12,9 +13,9 @@ public class PostService {
 
     public List<Post> listAllPosts(){
         ArrayList<Post> list = new ArrayList<>();
-        list.add(new Post("post1"));
-        list.add(new Post("post2"));
-        list.add(new Post("post3"));
+        list.add(new Post("post1", new Date()));
+        list.add(new Post("post2", new Date()));
+        list.add(new Post("post3", new Date()));
         return list;
     }
 }
